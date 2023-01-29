700fcbd48315d9cadf726ac119bd06ff9d789abe
dodor <doris.choi92@gmail.com>
2023-01-29 21:08:34 +0900

    Initial Commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Do-Young Choi on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
