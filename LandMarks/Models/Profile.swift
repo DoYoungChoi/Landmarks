5b906dd73aee07b62d502e7553282710f13a6ff9
dodor <doris.choi92@gmail.com>
2023-02-05 20:45:24 +0900

    🏗️App Design and Layout: Working with UI Controls - accessibilityLabel - toolbar, sheet, listStyle modifiers - @Environment(\.editMode) - TextField, Toggle, Picker, DatePicker View - onAppear(perform:), onDisappear(perform:) modifiers

---
+++ LandMarks/Models/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  LandMarks
+//
+//  Created by Do-Young Choi on 2023/02/05.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "dodor")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
